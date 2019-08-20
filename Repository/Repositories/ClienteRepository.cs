74b0b14fdb2949ea30eb95f433bb41494f471c84
paullohenriquecamargo <49409587+paullohenriquecamargo@users.noreply.github.com>
2019-08-20 14:44:52 -0300

    Criado a tabela clientes_logins.

--- Repository/Repositories/ClienteRepository.cs
+++ Repository/Repositories/ClienteRepository.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using Model;
+using Repository.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Repository.Repositories
 {
-    public class ClienteRepository
+    public class ClienteRepository : IClienteRepository
     {
+        private SistemContext context;
+
+        public ClienteRepository(SistemContext context)
+        {
+            this.context = context;
+        }
+
+        public bool Apagar(int idCliente)
+        {                                                
+        }
+
+        public Cliente ObterPeloId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> ObterTodosPeloIdCliente(int idCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Relacionar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
