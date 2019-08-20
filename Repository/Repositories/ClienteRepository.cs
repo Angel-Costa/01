8df6d88ea0bf979412f26cba9e51b054d2652f59
LoeneSilva <49409909+LoeneSilva@users.noreply.github.com>
2019-08-20 15:35:31 -0300

    Repositorio da Avaliacao

--- Repository/Repositories/ClienteRepository.cs
+++ Repository/Repositories/ClienteRepository.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Model;
+using Repository.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Repository.Repositories
 {
-    public class ClienteRepository
+    public class ClienteRepository : IClienteRepository
     {
+        public bool Apagar(int idCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPeloId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> ObterTodosPeloIdCliente(int idCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Relacionar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Repository/Repositories/ComodidadeRepository.cs
+++ Repository/Repositories/ComodidadeRepository.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using Model;
+using Repository.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Repository.Repositories
 {
-    public class ComodidadeRepository
+    public class ComodidadeRepository : IComodidadeRepository
     {
+        public bool Alterar(Comodidade comodidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Inserir(Comodidade comodidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comodidade ObterPeloId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comodidade> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
