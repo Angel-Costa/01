4df35338294c9a39482d43654f2f4f664687e4ec
paullohenriquecamargo <49409587+paullohenriquecamargo@users.noreply.github.com>
2019-08-20 16:55:03 -0300

    Repositorio do hotel.

--- Repository/Repositories/HotelRepository.cs
+++ Repository/Repositories/HotelRepository.cs
@@ -2,35 +2,54 @@
 using Repository.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Repository.Repositories
 {
     public class HotelRepository : IHotelRepository
     {
+        private SistemaContext context;
+        
+        public HotelRepository(SistemaContext context)
+        {
+            this.context = context;
+        }
+
         public bool Alterar(Hotel hotel)
         {
             hotel.RegistroAtivo = true;
+            context.Hoteis.Update(hotel);
+            return context.SaveChanges() == 1;
         }
 
         public bool Apagar(int id)
         {
-            throw new NotImplementedException();
+            var hotel = context.Hoteis.FirstOrDefault(y => y.Id == id);
+            if (hotel == null)
+                return false;
+
+            hotel.RegistroAtivo = false;
+            return context.SaveChanges() == 1;
         }
 
         public int Inserir(Hotel hotel)
         {
-            throw new NotImplementedException();
+            hotel.RegistroAtivo = true;
+            context.Hoteis.Add(hotel);
+            context.SaveChanges();
+            return hotel.Id;
         }
 
         public Hotel ObterPeloId(int id)
         {
-            throw new NotImplementedException();
+            return context.Hoteis.FirstOrDefault(x => x.Id == id && x.RegistroAtivo);
         }
 
         public List<Hotel> ObterTodos()
         {
-            throw new NotImplementedException();
+            return context.Hoteis
+                .Where(x => x.RegistroAtivo).ToList().ToList();
         }
     }
 }
--- Repository/SistemaContext.cs
+++ Repository/SistemaContext.cs
@@ -16,7 +16,7 @@ namespace Repository
         public DbSet<Avaliacao> Avaliacaos { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Comodidade> Comodidades { get; set; }
-        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Hotel> Hoteis { get; set; }
         public DbSet<Login> Logins { get; set; }
         public DbSet<ClienteLogin> ClientesLogins { get; set; }
 
